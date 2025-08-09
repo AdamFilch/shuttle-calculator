2d4dff4f193b694a35b24d950aa9a130eba0bb62
Adam Filchoir <filchoiradam@gmail.com>
2025-08-09 17:42:32 +0700

    chore(sample): create sample page for button the user may press and navigate to

---
+++ app/session/index.tsx
@@ -0,0 +1,13 @@
+import { ScrollView, Text, View } from "react-native";
+
+export default function SessionPage() {
+    return (
+        <ScrollView>
+            <View style={{
+                backgroundColor: 'white'
+            }}>
+                <Text>Sessions Page</Text>
+            </View>
+        </ScrollView>
+    )
+}
