02d867c32c9a7846fb01114f107d56c3688e4d0d
Adam Filchoir <filchoiradam@gmail.com>
2025-08-10 10:30:20 +0700

    remove other tabs only home)

--- app/session/index.tsx
+++ app/session/index.tsx
@@ -1,6 +1,8 @@
-import { ScrollView, Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { ScrollView, Text, TouchableOpacity, View, ViewStyle } from "react-native";
 
 export default function SessionPage() {
+    const router = useRouter() 
     return (
         <ScrollView>
             <View style={{
@@ -8,6 +10,34 @@ export default function SessionPage() {
             }}>
                 <Text>Sessions Page</Text>
             </View>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    gap: 10,
+                    // alignSelf: 'center',
+                    width: 'auto',
+                }}
+            >
+                <TouchableOpacity 
+                    onPress={() => {
+                        router.navigate('/session')
+                    }}
+                    style={buttonStyle}
+                >
+                    <Text>Go to Sessions</Text>
+                </TouchableOpacity>
+                {/* <TouchableOpacity style={buttonStyle}>
+                      <Text>Add User</Text>
+                    </TouchableOpacity> */}
+            </View>
         </ScrollView>
     )
+}
+
+const buttonStyle: ViewStyle = {
+  backgroundColor: 'lightgray',
+  width: 100,
+  height: 100,
+  justifyContent: 'center'
 }
---
+++ app/session/match/index.tsx
@@ -0,0 +1,12 @@
+import { ScrollView, View } from "react-native";
+
+
+export default function MatchPage() {
+    return (
+        <ScrollView>
+            <View>
+                Match Page
+            </View>
+        </ScrollView>
+    )
+}
