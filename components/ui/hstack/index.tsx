8fc6536f4124885313f757e074b641e7fc4276bb
Adam Filchoir <filchoiradam@gmail.com>
2025-09-25 16:10:51 +0700

    Revert hstack changes

--- components/ui/hstack/index.tsx
+++ components/ui/hstack/index.tsx
@@ -1,20 +1,16 @@
 import { VariantProps } from '@gluestack-ui/utils/nativewind-utils';
 import React from 'react';
-import type { ViewProps } from 'react-native';
 import { View } from 'react-native';
 import { hstackStyle } from './styles';
 
-type IHStackProps = ViewProps &
-  Omit<VariantProps<typeof hstackStyle>, "space"> & {
-    space?: VariantProps<typeof hstackStyle>["space"] | number;
-    className?: string;
-  };
 
+type IHStackProps = React.ComponentProps<typeof View> &
+  VariantProps<typeof hstackStyle>;
 const HStack = React.forwardRef<React.ComponentRef<typeof View>, IHStackProps>(
   function HStack({ className, space, reversed, ...props }, ref) {
     return (
       <View
-        className={`${hstackStyle({ space, reversed })} ${className ?? ""}`}
+        className={hstackStyle({ space, reversed, class: className })}
         {...props}
         ref={ref}
       />
