0f57026af17d4583c1fe6faaae344fbe83a076db
Adam Filchoir <filchoiradam@gmail.com>
2026-08-27 15:29:30 +0700

    add max length to players name

--- components/user/modal.tsx
+++ components/user/modal.tsx
@@ -1,90 +1,101 @@
-import { createPlayer } from "@/services/player"
-import { useState } from "react"
-import { Button, ButtonText } from "../ui/button"
-import { Heading } from "../ui/heading"
-import { CloseIcon, Icon } from "../ui/icon"
-import { Input, InputField } from "../ui/input"
-import { Modal, ModalBackdrop, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader } from "../ui/modal"
-import { Text } from "../ui/text"
-import { VStack } from "../ui/vstack"
-
+import { createPlayer } from "@/services/player";
+import { useState } from "react";
+import { Button, ButtonText } from "../ui/button";
+import { Heading } from "../ui/heading";
+import { CloseIcon, Icon } from "../ui/icon";
+import { Input, InputField } from "../ui/input";
+import {
+    Modal,
+    ModalBackdrop,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+} from "../ui/modal";
+import { Text } from "../ui/text";
+import { VStack } from "../ui/vstack";
 
 export function AddPlayerModal({
-    open,
-    onClose
+  open,
+  onClose,
 }: {
-    open: boolean,
-    onClose: () => void
+  open: boolean;
+  onClose: () => void;
 }) {
-    const [playername, setPlayername] = useState('')
+  const [playername, setPlayername] = useState("");
 
+  async function onClickSave() {
+    const res = await createPlayer(playername);
 
-    async function onClickSave() {
-        const res = await createPlayer(playername)
-
-        if (res) {
-            setPlayername('')
-            onClose()
-        }
+    if (res) {
+      setPlayername("");
+      onClose();
     }
+  }
 
-
-    return (
-        <Modal
-            isOpen={open}
-            onClose={() => {
-                onClose()
+  return (
+    <Modal
+      isOpen={open}
+      onClose={() => {
+        onClose();
+      }}
+    >
+      <ModalBackdrop />
+      <ModalContent>
+        <ModalHeader>
+          <Heading>Add a Player</Heading>
+          <ModalCloseButton>
+            <Icon
+              as={CloseIcon}
+              size="md"
+              className="stroke-background-400 group-[:hover]/modal-close-button:stroke-background-700 group-[:active]/modal-close-button:stroke-background-900 group-[:focus-visible]/modal-close-button:stroke-background-900"
+            />
+          </ModalCloseButton>
+        </ModalHeader>
+        <ModalBody>
+          <VStack space="sm">
+            <Text size="sm" className="text-typography-500">
+              Add a player here
+            </Text>
+            <Input
+              variant="outline"
+              size="md"
+              isDisabled={false}
+              isInvalid={false}
+              isReadOnly={false}
+            >
+              <InputField
+                defaultValue={playername}
+                maxLength={25}
+                value={playername}
+                onChangeText={(val) => {
+                  setPlayername(val);
+                }}
+                placeholder="Enter a Player Name"
+              />
+            </Input>
+          </VStack>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            variant="outline"
+            action="secondary"
+            onPress={() => {
+              onClose();
             }}
-        >
-            <ModalBackdrop />
-            <ModalContent>
-                <ModalHeader>
-                    <Heading>
-                        Add a Player
-                    </Heading>
-                    <ModalCloseButton>
-                        <Icon
-                            as={CloseIcon}
-                            size="md"
-                            className="stroke-background-400 group-[:hover]/modal-close-button:stroke-background-700 group-[:active]/modal-close-button:stroke-background-900 group-[:focus-visible]/modal-close-button:stroke-background-900"
-                        />
-                    </ModalCloseButton>
-                </ModalHeader>
-                <ModalBody>
-                    <VStack space="sm">
-                        <Text size="sm" className="text-typography-500">Add a player here</Text>
-                        <Input
-                            variant="outline"
-                            size="md"
-                            isDisabled={false}
-                            isInvalid={false}
-                            isReadOnly={false}
-                        >
-                            <InputField defaultValue={playername} value={playername} onChangeText={(val) => {
-                                setPlayername(val)
-                            }} placeholder="Enter a Player Name" />
-                        </Input>
-                    </VStack>
-                </ModalBody>
-                <ModalFooter>
-                    <Button
-                        variant="outline"
-                        action="secondary"
-                        onPress={() => {
-                            onClose()
-                        }}
-                    >
-                        <ButtonText>Cancel</ButtonText>
-                    </Button>
-                    <Button
-                        onPress={() => {
-                            onClickSave()
-                        }}
-                    >
-                        <ButtonText>Add Player</ButtonText>
-                    </Button>
-                </ModalFooter>
-            </ModalContent>
-        </Modal>
-    )
+          >
+            <ButtonText>Cancel</ButtonText>
+          </Button>
+          <Button
+            onPress={() => {
+              onClickSave();
+            }}
+          >
+            <ButtonText>Add Player</ButtonText>
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
